8df759e8aca308da30cd421b62209a3e5375feca
Heder Alves <alvesheder92@gmail.com>
2024-03-21 07:50:18 -0300

    style: position function

--- src/store/index.js
+++ src/store/index.js
@@ -1,64 +1,62 @@
 import { createStore } from 'vuex';
 
-const state = {
-  account: {
-    id: '1',
-    name: "heder",
-    value: 304.78
+const store = createStore({
+  state: {
+    account: {
+      id: '1',
+      name: "heder",
+      value: 304.78
+    },
+    cardsInfo: [
+      {
+        id: '1',
+        title: "Conta PJ",
+        value: 39.828
+      },
+      {
+        id: '2',
+        title: "Recebimentos",
+        value: 765.88
+      },
+      {
+        id: '3',
+        title: "Lucro",
+        value: 15.887
+      }
+    ],
+    recentTransactions: [
+      {
+        id: '1',
+        name: "Mercadinho da Cidade",
+        value: 10.98,
+      },
+      {
+        id: '2',
+        name: "Tenda Nostra",
+        value: 26.38,
+      },
+      {
+        id: '3',
+        name: "Farmácia Um",
+        value: 43.17,
+      },
+      {
+        id: '4',
+        name: "Academia Body",
+        value: 62.12,
+      },
+      {
+        id: '5',
+        name: "ShopOnline",
+        value: 134.24,
+      }
+    ],
+    payments: [],
+    totalBalance: {
+      id: 'w4s7e8d4f455q',
+      value: 1
+    }
   },
-  cardsInfo: [
-    {
-      id: '1',
-      title: "Conta PJ",
-      value: 39.828
-    },
-    {
-      id: '2',
-      title: "Recebimentos",
-      value: 765.88
-    },
-    {
-      id: '3',
-      title: "Lucro",
-      value: 15.887
-    }
-  ],
-  recentTransactions: [
-    {
-      id: '1',
-      name: "Mercadinho da Cidade",
-      value: 10.98,
-    },
-    {
-      id: '2',
-      name: "Tenda Nostra",
-      value: 26.38,
-    },
-    {
-      id: '3',
-      name: "Farmácia Um",
-      value: 43.17,
-    },
-    {
-      id: '4',
-      name: "Academia Body",
-      value: 62.12,
-    },
-    {
-      id: '5',
-      name: "ShopOnline",
-      value: 134.24,
-    }
-  ],
-  payments: [],
-  totalBalance: {
-    id: 'w4s7e8d4f455q',
-    value: 1
-  }
-};
-
-export default createStore({
-  state,
   getters: {},
   mutations: {
     updatedBalance(state, data) {
@@ -68,3 +66,5 @@ export default createStore({
   actions: {},
   modules: {}
 });
+
+export default store;
