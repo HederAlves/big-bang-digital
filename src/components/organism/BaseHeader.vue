77fa8a357a869000c437196692f1b0eb0d1922e4
Heder Alves <alvesheder92@gmail.com>
2024-03-20 22:27:36 -0300

    feat: create base header

---
+++ src/components/organism/BaseHeader.vue
@@ -0,0 +1,37 @@
+<template>
+    <header class="header">
+        <img src="@/assets/big-bang.svg" />
+        <NavMenu />
+        <BaseButton color="primary" size="sm" @click="navigate('page Notification')">
+            <img src="@/assets/notification.svg" />
+        </BaseButton>
+    </header>
+</template>
+
+<script>
+import BaseButton from '../atoms/BaseButton.vue';
+import NavMenu from '../molecules/NavMenu';
+
+export default {
+  name: 'BaseHeader',
+  components: {
+    NavMenu,
+    BaseButton,
+  },
+  methods: {
+    navigate(page) {
+      console.log("Nav to:", page);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.header {
+    display: flex;
+    justify-content: space-between;
+    padding: 15px;
+    background-color: #202027;
+    color: white;
+}
+</style>
