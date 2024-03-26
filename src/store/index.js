c1626ec409465a19224c7ef269cd245ab97104fd
Heder Alves <alvesheder92@gmail.com>
2024-03-26 05:03:07 -0300

    feat: add new value account

--- src/store/index.js
+++ src/store/index.js
@@ -1,65 +1,65 @@
-import { createStore } from 'vuex';
+import { createStore } from "vuex";
 
 const store = createStore({
   state: {
     account: {
-      id: '1',
+      id: "1",
       name: "heder",
       value: 304.78
     },
     cardsInfo: [
       {
-        id: '1',
+        id: "1",
         title: "Conta PJ",
         value: 39.828
       },
       {
-        id: '2',
+        id: "2",
         title: "Recebimentos",
         value: 765.88
       },
       {
-        id: '3',
+        id: "3",
         title: "Lucro",
         value: 15.887
       }
     ],
     recentTransactions: [
       {
-        id: '1',
+        id: "1",
         name: "Mercadinho da Cidade",
         value: 10.98,
-        backgroundColor: '#FDFF9D'
+        backgroundColor: "#FDFF9D"
       },
       {
-        id: '2',
+        id: "2",
         name: "Tenda Nostra",
         value: 26.38,
-        backgroundColor: '#FF9D9D'
+        backgroundColor: "#FF9D9D"
       },
       {
-        id: '3',
+        id: "3",
         name: "Farmácia Um",
         value: 43.17,
-        backgroundColor: '#C8FF9D'
+        backgroundColor: "#C8FF9D"
       },
       {
-        id: '4',
+        id: "4",
         name: "Academia Body",
         value: 62.12,
-        backgroundColor: '#AB9DFF'
+        backgroundColor: "#AB9DFF"
       },
       {
-        id: '5',
+        id: "5",
         name: "ShopOnline",
         value: 134.24,
-        backgroundColor: '#FF9DEF'
+        backgroundColor: "#FF9DEF"
       }
     ],
-    payments: [],
-    totalBalance: {
-      id: 'w4s7e8d4f455q',
-      value: 1
+    movement: {
+      id: "1",
+      earnings: 2789.21,
+      exits: 1278.47
     }
   },
   getters: {},
@@ -68,8 +68,6 @@ const store = createStore({
       state.totalBalance = data;
     }
   },
-  actions: {},
-  modules: {}
 });
 
 export default store;
